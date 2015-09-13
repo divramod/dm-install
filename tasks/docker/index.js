58b7d5cb6e78758237c5abc6a775966ebaa4bab6
divramod <divramod@gmail.de>
2015-09-13 16:50:23 +0200

    added docker

--- tasks/docker/index.js
+++ tasks/docker/index.js
@@ -1,14 +1,29 @@
 // =========== [ REQUIRE ] ===========
 var co = require("co");
+var spawn = require("dm-shell").spawn;
 
 // =========== [ MODULE DEFINE ] ===========
 var task = {};
 
 // =========== [ SYNC task.start() ] ===========
-task.start = function(p1) {
+task.start = function() {
     try {
-        var p1 = p1 || process.argv[3] || undefined;
-        return p1;
+        // =========== [ docker ] ===========
+        var command = "";
+        command = "sudo apt-get install curl";
+        command += " && curl -sSL https://get.docker.com/ | sh";
+        spawn(command);
+
+        // =========== [ docker compose ] ===========
+        command = "curl -L https://github.com/docker/compose/releases/download/VERSION_NUM/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose";
+        command += " && chmod +x /usr/local/bin/docker-compose";
+        command += " && docker-compose --version";
+        spawn(command);
+
+        // =========== [ command completion ] ===========
+
+
+        return "test";
     } catch (e) {
         console.log("Filename: ", __filename, "\n", e.stack);
         return e;
